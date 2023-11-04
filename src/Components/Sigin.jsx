cb20eea93711ea287ede4271de3f5ae4afa6050c
Rhon-Vincent <134147488+Rhon-Vincent@users.noreply.github.com>
2023-11-04 22:26:06 +0800

    new

--- src/Components/Sigin.jsx
+++ src/Components/Sigin.jsx
@@ -1,10 +1,15 @@
 const Sigin = () => {
+  const signIn = {
+    signIn: "Sign In",
+    password: "t",
+  };
   return (
     <div className="Container">
-      <h1>{heading}</h1>
-      <input type="text" placeholder="Email" />
+      <h1>SignIn</h1>
+      <input type="email" placeholder="Email" />
       <input type="password" placeholder="Password" />
       <button>Click me</button>
+      <input type="checkbox" /> remember me
     </div>
   );
 };
