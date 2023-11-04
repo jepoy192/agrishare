3a8065e773660c965c105c8410ed5204c98fa14e
leahparD <149226688+leahparD@users.noreply.github.com>
2023-11-05 00:38:40 +0800

    navbar

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -3,14 +3,14 @@ import "../Styles/navbar.css";
 const Navbar = () => {
   return (
   <div className="button">
-    <button class="Home">Home</button>
+    <button class="Home" >Home</button>
     <button class="Trade">Trade</button>
     <button class="Donation">Donation</button>
     <button class="Points">Points</button>
-    <button class="Donation">Contact Us</button>
     <button class="Points">About Us</button>
+    <button class="Donation">Contact Us</button>
 </div>
-
+    
   )
 }
 
