f0f4da7ef652c918ac8cdd17f75d543999b8a2e0
Rhon-Vincent <134147488+Rhon-Vincent@users.noreply.github.com>
2023-11-05 00:03:18 +0800

    updated-sign form

--- src/Components/Sigin.jsx
+++ src/Components/Sigin.jsx
@@ -1,12 +1,15 @@
 import "../Styles/signin.css";
+import passicon from "../Images/icons8-eye-16 (1).png";
 
 const Sigin = () => {
   return (
     <div className="container">
-      <h1 className="heading1">SignIn</h1>
-      <br />
-      <input type="email" placeholder="Email" className="email" /> <br />
-      <input type="password" placeholder="Password" className="password" />
+      <h1 className="heading1">Sign In</h1>
+      <div>
+        <input type="email" placeholder="Email" className="email" />
+        <input type="password" placeholder="Password" className="password" />
+        {/* <img src={passicon} className="icon" alt="Password Icon" /> */}
+      </div>
       <br />
       <input type="checkbox" className="checkBox" /> remember me
       <span className="Forgot">Forgot Password</span>
